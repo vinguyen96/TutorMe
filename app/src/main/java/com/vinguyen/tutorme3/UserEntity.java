2de9188742f49ccb001f7bad2250562adc52e0f2
vinguyen <vinguyen96@outlook.com.au>
2017-09-02 00:17:05 +1000

    MainActivity Text + Image works :O

---
+++ app/src/main/java/com/vinguyen/tutorme3/UserEntity.java
@@ -0,0 +1,54 @@
+package com.vinguyen.tutorme3;
+
+/**
+ * Created by vinguyen on 31/8/17.
+ */
+
+public class UserEntity {
+    public String name;
+    public String age;
+    public String degree;
+    public String contact;
+
+    public UserEntity() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public UserEntity(String name, String age, String degree, String contact) {
+        this.name = name;
+        this.age = age;
+        this.degree = degree;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+}
