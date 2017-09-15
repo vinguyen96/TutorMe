1f5df043f6427fa694e6ed56f124b65278690280
vinguyen <vinguyen96@outlook.com.au>
2017-09-15 14:02:03 +1000

    Click on tutor

--- app/src/main/java/com/vinguyen/tutorme3/FindTutorFragment.java
+++ app/src/main/java/com/vinguyen/tutorme3/FindTutorFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,20 +25,22 @@ import java.util.ArrayList;
 public class FindTutorFragment extends Fragment {
 
     private ListView listView;
-    private ArrayAdapter arrayAdapter;
+    private ArrayAdapter arrayAdapter, arrayAdapterID;
     DatabaseReference databaseRef;
     ArrayList<String> tutors;
+    ArrayList<String> tutorsID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_find_tutor, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_find_tutor, container, false);
 
         databaseRef = FirebaseDatabase.getInstance().getReference();
         listView = (ListView)rootView.findViewById(R.id.listView);
 
         tutors = new ArrayList<String>();
+        tutorsID = new ArrayList<String>();
         ValueEventListener valueEventListener = databaseRef.child("Courses").child("INFS1609").child("Tutors").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -49,6 +52,22 @@ public class FindTutorFragment extends Fragment {
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Bundle bundle = new Bundle();
+                bundle.putString("message", tutorsID.get(position));
+                UserViewFragment fragment = new UserViewFragment();
+                fragment.setArguments(bundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                //Toast.makeText(getActivity(), tutorsID.get(position), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -83,6 +102,7 @@ public class FindTutorFragment extends Fragment {
                         userEntity.setName(ds.child(key).getValue(UserEntity.class).getName());
                         userEntity.setAge(ds.child(key).getValue(UserEntity.class).getAge());
                         tutors.add(userEntity.getName() + " " + userEntity.getAge());
+                        tutorsID.add(key);
                     }
                 }
 
---
+++ app/src/main/java/com/vinguyen/tutorme3/UserViewFragment.java
@@ -0,0 +1,109 @@
+package com.vinguyen.tutorme3;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.signature.StringSignature;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
+public class UserViewFragment extends Fragment {
+
+    private FirebaseDatabase userDatabase;
+    private DatabaseReference userReference;
+    private TextView profileName, profileAge, profileDegree, profileContact;
+    private ImageView imgView;
+    String userID;
+    private StorageReference myRef, defaultRef, storageRef;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_user_view, container, false);
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            userID = bundle.getString("message");
+        }
+
+        profileName=(TextView) rootView.findViewById(R.id.profileName);
+        profileAge=(TextView) rootView.findViewById(R.id.profileAge);
+        profileDegree=(TextView) rootView.findViewById(R.id.profileDegree);
+        profileContact=(TextView) rootView.findViewById(R.id.profileContact);
+
+        imgView=(ImageView) rootView.findViewById(R.id.imgView);
+
+        userDatabase = FirebaseDatabase.getInstance();
+        userReference = userDatabase.getReference();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null && userID != null) {
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            storageRef = storage.getReferenceFromUrl("gs://tutorme-61083.appspot.com/userProfileImages");
+            myRef = storageRef.child(userID + ".jpg");
+            defaultRef = storageRef.child("default.jpg");
+            refreshImage();
+            userReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Log.d("CHECKPOINT", userID);
+                    showData(dataSnapshot);
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) { }
+            });
+        }
+        return rootView;
+    }
+
+    private void showData(DataSnapshot dataSnapshot){
+        for(DataSnapshot ds:dataSnapshot.getChildren()){
+            UserEntity userEntity=new UserEntity();
+            if (ds.hasChild(userID)){
+                userEntity.setName(ds.child(userID).getValue(UserEntity.class).getName());
+                userEntity.setAge(ds.child(userID).getValue(UserEntity.class).getAge());
+                userEntity.setDegree(ds.child(userID).getValue(UserEntity.class).getDegree());
+                userEntity.setContact(ds.child(userID).getValue(UserEntity.class).getContact());
+
+                profileName.setText("Name: " + userEntity.getName());
+                profileAge.setText("Age: " + userEntity.getAge());
+                profileDegree.setText("Degree: " + userEntity.getDegree());
+                profileContact.setText("Contact: " + userEntity.getContact());
+            }
+        }
+
+    }
+
+    public void refreshImage() {
+        if (getActivity() != null) {
+            myRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Glide.with(getActivity()).using(new FirebaseImageLoader()).load(myRef).signature(new StringSignature(String.valueOf(System.currentTimeMillis()))).into(imgView);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Glide.with(getActivity()).using(new FirebaseImageLoader()).load(defaultRef).signature(new StringSignature(String.valueOf(System.currentTimeMillis()))).into(imgView);
+                }
+            });
+        }
+    }
+}
